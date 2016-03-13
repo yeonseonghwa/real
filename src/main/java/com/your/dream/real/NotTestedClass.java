56dce864b7c5de2194e8fbbcf67683de9c43dfab
tchi <tchi@riotgames.com>
2016-03-13 14:51:48 +0900

    New file

---
+++ src/main/java/com/your/dream/real/NotTestedClass.java
@@ -0,0 +1,11 @@
+package com.your.dream.real;
+
+public class NotTestedClass {
+	public NotTestedClass() {
+	}
+	
+	@Override
+	public String toString() {
+		return "this is not tested yet.";
+	}
+}
