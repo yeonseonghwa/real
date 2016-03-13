c5426bd9076899adfd9e4afef9d478fa1c7fe9ff
tchi <tchi@riotgames.com>
2016-03-13 17:19:05 +0900

    Change simple one

--- src/main/java/com/your/dream/real/App.java
+++ src/main/java/com/your/dream/real/App.java
@@ -5,6 +5,10 @@ package com.your.dream.real;
  *
  */
 public class App {
+	private App() {
+		// do not create App class directly
+	}
+	
     public static void main( String[] args ) {
     	for(String arg : args) {
     		System.out.println("parameter: " + arg);
